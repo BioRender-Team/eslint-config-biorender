7e9b8ec4074ba86596b1ea5db9d33e2ba86536fe
Omar Chehab <omarchehab98@gmail.com>
2019-03-28 11:13:48 -0400

    Fix cypress config env

--- cypress.js
+++ cypress.js
@@ -1,12 +1,9 @@
 module.exports = {
     env: {
-        es6: true,
-        'cypress/globals': true,
-    },
-    env: {
         browser: true, // we are targeting browser environments
         es6: true, // we use es6
         node: true, // we use node
+        'cypress/globals': true,
     },
     extends: [
         'plugin:cypress/recommended',
