ce55af15be0af8a4ea51450b421e2303738468d3
Rodney Draaisma <virtualcodewarrior@hotmail.com>
2020-08-26 17:30:44 -0400

    react-hooks is a separate package apparently

--- react.js
+++ react.js
@@ -5,10 +5,12 @@ module.exports = {
     },
     extends: [
         'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
         'plugin:jsx-a11y/recommended',
     ],
     plugins: [
         'react',
+        'react-hooks',
         'jsx-a11y',
     ],
     settings: {
